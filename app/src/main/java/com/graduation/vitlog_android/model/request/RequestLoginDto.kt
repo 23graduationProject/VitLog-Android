90f91d82a449d6fc71bbffd63ba856b0cae5a5dc
HyeseonBaek <konkukbhs@gmail.com>
2024-02-26 17:43:21 +0900

    [add] 로그인 요청, 응답 DTO

--- app/src/main/java/com/graduation/vitlog_android/model/request/RequestLoginDto.kt
+++ app/src/main/java/com/graduation/vitlog_android/model/request/RequestLoginDto.kt
@@ -1,7 +1,9 @@
 package com.graduation.vitlog_android.model.request
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class RequestLoginDto(
     @SerialName("user_id")
     val user_id : String,
--- app/src/main/java/com/graduation/vitlog_android/model/response/ResponseSignUpDto.kt
+++
@@ -1,5 +0,0 @@
-package com.graduation.vitlog_android.model.response
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
