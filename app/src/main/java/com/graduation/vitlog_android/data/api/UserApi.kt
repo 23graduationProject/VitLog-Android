40c4da4b7d8444881f14df3fab92428d6af13212
HyeseonBaek <konkukbhs@gmail.com>
2024-02-26 15:21:32 +0900

    [feat] 로그인, 회원가입 API, Repository

---
+++ app/src/main/java/com/graduation/vitlog_android/data/api/UserApi.kt
@@ -0,0 +1,18 @@
+package com.graduation.vitlog_android.data.api
+
+import com.graduation.vitlog_android.model.request.RequestLoginDto
+import com.graduation.vitlog_android.model.request.RequestSignUpDto
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface UserApi {
+    @POST("api/user/signup")
+    suspend fun postSignUp(
+        @Body requestSignUpDto : RequestSignUpDto
+    ): Unit
+
+    @POST("api/user/login")
+    suspend fun postLogin(
+        @Body requestLoginDto : RequestLoginDto
+    ): Unit
+}
---
+++ app/src/main/java/com/graduation/vitlog_android/data/repository/UserRepository.kt
@@ -0,0 +1,22 @@
+package com.graduation.vitlog_android.data.repository
+
+import com.graduation.vitlog_android.data.api.UserApi
+import com.graduation.vitlog_android.model.request.RequestLoginDto
+import com.graduation.vitlog_android.model.request.RequestSignUpDto
+import javax.inject.Inject
+
+class UserRepository @Inject constructor(
+    private val api: UserApi
+) {
+    suspend fun postSignUp(
+        requestSignUpDto: RequestSignUpDto
+    ): Result<Unit> = runCatching {
+        api.postSignUp(requestSignUpDto)
+    }
+
+    suspend fun postLogin(
+        requestLoginDto: RequestLoginDto
+    ): Result<Unit> = runCatching {
+        api.postLogin(requestLoginDto)
+    }
+}
