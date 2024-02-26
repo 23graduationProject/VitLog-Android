15754a7b55c032d56be49c86463bc4b993f852b8
HyeseonBaek <konkukbhs@gmail.com>
2024-02-26 16:35:27 +0900

    [mod] Dto 변경

--- app/src/main/java/com/graduation/vitlog_android/model/request/RequestLoginDto.kt
+++ app/src/main/java/com/graduation/vitlog_android/model/request/RequestLoginDto.kt
@@ -3,7 +3,7 @@ package com.graduation.vitlog_android.model.request
 import kotlinx.serialization.SerialName
 
 data class RequestLoginDto(
-    @SerialName("userId")
+    @SerialName("user_id")
     val user_id : String,
     @SerialName("password")
     val password : String
