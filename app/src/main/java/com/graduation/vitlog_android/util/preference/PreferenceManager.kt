d634d2f4d3450157992e9b8ff0eb0f64b32f207c
hyeseon <konkukbhs@gmail.com>
2024-03-16 22:43:25 +0900

    [add] PreferenceManager

---
+++ app/src/main/java/com/graduation/vitlog_android/util/preference/PreferenceManager.kt
@@ -0,0 +1,36 @@
+package com.graduation.vitlog_android.util.preference
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object SharedPrefManager {
+    private const val PREFS_NAME = "MyPrefsFile"
+    private lateinit var preferences: SharedPreferences
+
+    fun init(context: Context) {
+        preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+    }
+
+    fun save(key: String, value: Any) {
+        val editor = preferences.edit()
+        when (value) {
+            is Boolean -> editor.putBoolean(key, value)
+            is Float -> editor.putFloat(key, value)
+            is Int -> editor.putInt(key, value)
+            is Long -> editor.putLong(key, value)
+            is String -> editor.putString(key, value)
+            else -> error("Unsupported data type")
+        }
+        editor.apply()
+    }
+
+    fun getString(key: String, defaultValue: String = ""): String =
+        preferences.getString(key, defaultValue) ?: defaultValue
+
+    fun getInt(key: String, defaultValue: Int = 0): Int =
+        preferences.getInt(key, defaultValue)
+
+    fun getBoolean(key: String, defaultValue: Boolean = false): Boolean =
+        preferences.getBoolean(key, defaultValue)
+
+}
