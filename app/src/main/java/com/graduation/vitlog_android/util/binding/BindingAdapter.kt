80c8da74d26ddbd47df9298adf19b1b07504cdbe
hyeseon <konkukbhs@gmail.com>
2024-05-14 16:16:02 +0900

    유저 아이디, 대표 사진 bindingAdapter

--- app/src/main/java/com/graduation/vitlog_android/util/binding/BindingAdapter.kt
+++ app/src/main/java/com/graduation/vitlog_android/util/binding/BindingAdapter.kt
@@ -4,10 +4,16 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.RoundedCornersTransformation
+import com.graduation.vitlog_android.R
 
 @BindingAdapter("setImageUrl")
-fun ImageView.setImageUrl(imageUrl: String) {
-    load(imageUrl) {
-        transformations(RoundedCornersTransformation(20F))
+fun ImageView.setImageUrl(imageUrl: String?) {
+    if (imageUrl != null) {
+        load(imageUrl) {
+            transformations(RoundedCornersTransformation(200F))
+        }
+    } else {
+        load(R.drawable.img_mypage_profile)
     }
 }
+
