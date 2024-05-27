e9d8ca6755aad2fcbbafa7dd391cc238746bcb99
hyeseon <konkukbhs@gmail.com>
2024-05-28 01:05:59 +0900

    [feat] 바인딩어댑터 서버로부터의 url / 로컬이미지 / 기본이미지 분기처리해 구현

--- app/src/main/java/com/graduation/vitlog_android/util/binding/BindingAdapter.kt
+++ app/src/main/java/com/graduation/vitlog_android/util/binding/BindingAdapter.kt
@@ -9,11 +9,27 @@ import com.graduation.vitlog_android.R
 @BindingAdapter("setImageUrl")
 fun ImageView.setImageUrl(imageUrl: String?) {
     if (imageUrl != null) {
-        load(imageUrl) {
-            transformations(RoundedCornersTransformation(200F))
+        if (imageUrl.startsWith("http")) {
+            // URL인 경우
+            load(imageUrl) {
+                transformations(RoundedCornersTransformation(200F))
+            }
+        } else {
+            // 로컬 리소스인 경우
+            val resourceId = context.resources.getIdentifier(
+                imageUrl, "drawable", context.packageName
+            )
+            if (resourceId != 0) {
+                load(resourceId) {
+                    transformations(RoundedCornersTransformation(200F))
+                }
+            } else {
+                load(R.drawable.img_mypage_profile)
+            }
         }
     } else {
         load(R.drawable.img_mypage_profile)
     }
 }
+
 
