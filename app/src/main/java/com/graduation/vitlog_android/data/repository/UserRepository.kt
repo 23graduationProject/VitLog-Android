560c3ced82baf8d585ee362073b74032c0ff7e12
hyeseon <konkukbhs@gmail.com>
2024-05-28 00:29:25 +0900

    [feat] postFace API, Repository 구현

--- app/src/main/java/com/graduation/vitlog_android/data/repository/UserRepository.kt
+++ app/src/main/java/com/graduation/vitlog_android/data/repository/UserRepository.kt
@@ -5,6 +5,7 @@ import com.graduation.vitlog_android.model.entity.User
 import com.graduation.vitlog_android.model.request.RequestLoginDto
 import com.graduation.vitlog_android.model.request.RequestSignUpDto
 import com.graduation.vitlog_android.model.response.ResponseLoginDto
+import okhttp3.MultipartBody
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
@@ -23,8 +24,16 @@ class UserRepository @Inject constructor(
     }
 
     suspend fun getUser(
-        uid : Int
+        uid: Int
     ): Result<User> = runCatching {
         api.getUser(uid).convertToUser()
+    }
+
+    suspend fun postFace(
+        uid: Int,
+        file: MultipartBody.Part?,
+        pName: String
+    ): Result<Unit> = runCatching {
+        api.postFace(uid, pName = pName, file = file)
     }
 }
