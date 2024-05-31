9fd67d5bbc4de077e7f6902713dd0d82d76d1f59
hyeseon <konkukbhs@gmail.com>
2024-06-01 00:54:36 +0900

    [feat] 최근 갤러리 영상 불러오는 것으로 변경 + 영상 썸네일 클릭 시 uri 넘겨 편집으로 이동

--- app/src/main/java/com/graduation/vitlog_android/presentation/home/GalleryAdapter.kt
+++ app/src/main/java/com/graduation/vitlog_android/presentation/home/GalleryAdapter.kt
@@ -1,18 +1,36 @@
 package com.graduation.vitlog_android.presentation.home
 
+import android.content.ContentUris
+import android.content.Context
 import android.net.Uri
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.graduation.vitlog_android.databinding.ItemGalleryImageBinding
 
-class GalleryAdapter(private val imageUris: List<Uri>) :
-    RecyclerView.Adapter<GalleryAdapter.GalleryImageViewHolder>() {
+class GalleryAdapter(
+    private val context: Context,
+    private val imageUris: List<Uri>,
+    private val onItemClicked: (Uri) -> Unit
+) : RecyclerView.Adapter<GalleryAdapter.GalleryImageViewHolder>() {
 
     class GalleryImageViewHolder(private val binding: ItemGalleryImageBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(uri: Uri) {
-            binding.image.setImageURI(uri)
+        fun bind(context: Context, uri: Uri, onItemClicked: (Uri) -> Unit) {
+            // 썸네일 불러오기
+            val bitmap = MediaStore.Video.Thumbnails.getThumbnail(
+                context.contentResolver,
+                ContentUris.parseId(uri),
+                MediaStore.Video.Thumbnails.MINI_KIND,
+                null
+            )
+            binding.image.setImageBitmap(bitmap)
+
+            // 클릭 이벤트 처리
+            itemView.setOnClickListener {
+                onItemClicked(uri)
+            }
         }
     }
 
@@ -23,8 +41,9 @@ class GalleryAdapter(private val imageUris: List<Uri>) :
     }
 
     override fun onBindViewHolder(holder: GalleryImageViewHolder, position: Int) {
-        holder.bind(imageUris[position])
+        holder.bind(context, imageUris[position], onItemClicked)
     }
 
     override fun getItemCount(): Int = imageUris.size
 }
+
